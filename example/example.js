63159cbb65238c1e0cf4efe94f62cf4eeb321200
Erik Lundin <erik@wub.io>
2013-05-17 10:50:22 +0100

    Add a simple example

---
+++ example/example.js
@@ -0,0 +1,32 @@
+var fs = require('fs')
+  , join = require('path').join
+  , wheelbarrow = require('../')
+
+// this function will be used to resolve all detected
+// `require()` calls
+function load(from, name, callback) {
+  if (name === 'message') {
+    return callback(null, { src: 'module.exports = "magic!"' })
+  }
+
+  // default to the built-in module loading function
+  wheelbarrow.load(from, name, callback)
+}
+
+// a.js will be our point of entry
+var path = join(__dirname, 'a.js')
+  , input =
+    [ { path: path
+      , src: fs.readFileSync(path, 'utf8')
+      }
+    ]
+
+// bundle it all up!
+wheelbarrow(input, { load: load }, function (err, payload) {
+  if (err != null) {
+    throw err
+  }
+
+  // eval the generated payload #YOLO
+  eval(payload)
+})
